55fffc6174a563fc8a5410269e125a437c4ead91
Yasser <Yasser@Git.com>
2020-05-19 05:07:38 +0300

    Add experience

--- routes/profile.js
+++ routes/profile.js
@@ -106,7 +106,6 @@ router.get('/user/:user_id', async (req, res) => {
 // @route   DELETE api/profile
 // @desc    Delete profile, user, posts
 // @access  Private
-
 router.delete('/', auth, async (req, res) => {
   try {
     // @todo - remove user posts
@@ -123,6 +122,49 @@ router.delete('/', auth, async (req, res) => {
     res.status(500).send('Server error');
   }
 });
+
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('company', 'Company is required').not().isEmpty(),
+      check('from', 'From date is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      descrition,
+    } = req.body;
+
+    const newExp = { title, company, location, from, to, current, descrition };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.experience = [newExp, ...profile.experience];
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 function buildProfileObject(req) {
   const {
