feaf865fa6f423ce62499a6a5a4208704f467e4d
Yasser <Yasser@Git.com>
2020-05-21 00:13:18 +0300

    Add Register & Login components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React, { Fragment } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 const App = () => (
   <Fragment>
     <Navbar />
-    <Landing />
+    <Switch>
+      <Route path='/' exact component={Landing} />
+      <Route exact path='/register' component={Register} />
+      <Route exact path='/login' component={Login} />
+    </Switch>
   </Fragment>
 );
 
