52c9d6d4ad4581e4548db70511ee0af35f5ac042
Yasser <Yasser@Git.com>
2020-05-20 06:00:34 +0300

    Add & remove comment

--- routes/posts.js
+++ routes/posts.js
@@ -151,4 +151,76 @@ router.put('/unlike/:id', auth, async (req, res) => {
   }
 });
 
+// @route   POST api/posts/comment/:id
+// @desc    Comment on a post
+// @access  Private
+router.post(
+  '/comment/:id',
+  [auth, [check('text', 'Text is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: erros.array() });
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const post = await Post.findById(req.params.id);
+
+      if (!post) return res.status(404).json({ msg: 'Post not found' });
+
+      const comment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      post.comments = [comment, ...post.comments];
+
+      await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      if (err.kind === 'ObjectId')
+        return res.status(404).json({ msg: 'Post not found' });
+
+      res.status(500).send('Server error');
+    }
+  }
+);
+
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Delete comment from a post
+// @access  Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) return res.status(404).json({ msg: 'Post not found' });
+
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+    if (!comment) return res.status(404).json({ msg: 'Comment not found' });
+
+    if (comment.user.toString() !== req.user.id)
+      return res.status(401).json({ msg: 'User not authorized' });
+
+    post.comments = post.comments.filter(
+      (comment) => comment.user.toString() !== req.user.id
+    );
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId')
+      return res.status(404).json({ msg: 'Resource not found' });
+
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
