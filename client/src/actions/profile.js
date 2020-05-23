bff8f8c0f6f9230b305be80e249ce884c87212f6
Yasser <Yasser@Git.com>
2020-05-23 17:52:56 +0300

    Add education and experiences

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { setAlert } from './alert';
 
-import { GET_PROFILE, PROFILE_ERROR } from './types';
+import { GET_PROFILE, UPDATE_PROFILE, PROFILE_ERROR } from './types';
 
 const api = 'http://localhost:5000/api';
 
@@ -42,6 +42,66 @@ export const createProfile = (fromData, history, edit = false) => async (
     dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
 
     if (!edit) history.push('/dashboard');
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors)
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+export const addExperience = (formData, history) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const res = await axios.put(`${api}/profile/experience`, formData, config);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+
+    dispatch(setAlert('Experience Added', 'success'));
+
+    history.push('/dashboard');
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors)
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+export const addEducation = (formData, history) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const res = await axios.put(`${api}/profile/education`, formData, config);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+
+    dispatch(setAlert('Education Added', 'success'));
+
+    history.push('/dashboard');
   } catch (err) {
     const errors = err.response.data.errors;
     if (errors)
--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -10,7 +10,7 @@ const DashboardActions = () => {
       <Link to='/add-experience' className='btn btn-light'>
         <i className='fab fa-black-tie text-primary'></i> Add Experience
       </Link>
-      <Link to='/ add-education' className='btn btn-light'>
+      <Link to='/add-education' className='btn btn-light'>
         <i className='fas fa-graduation-cap text-primary'></i> Add Education
       </Link>
     </div>
